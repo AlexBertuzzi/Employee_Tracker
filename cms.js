bd657d9f2fc2fca02c65166749b6439543314278
AlexBertuzzi <alexmichaelbertuzzi@yahoo.com>
2021-01-19 13:34:35 -0600

    adding directory function

--- cms.js
+++ cms.js
@@ -17,3 +17,49 @@ connection.connect(function(err) {
     startUp();
 });
 
+function startUp() {
+    inquirer
+        .prompt({
+            name: "action",
+            type: "rawlist",
+            message: "Choose what you would like to do.",
+            choices: [
+                "View Departments",
+                "View Roles",
+                "View Employees",
+                "Add Departments",
+                "Add Roles",
+                "Add Employees",
+                "Update Employee Roles",
+                "Exit"
+            ]
+        })
+        .then(function(answer) {
+            switch (answer.action) {
+                case "View Departments":
+                    viewDepartments();
+                    break;
+                case "View Roles":
+                    viewRoles();
+                    break;
+                case "View Employees":
+                    viewEmployees();
+                    break;
+                case "Add Departments":
+                    addDepartments();
+                    break;
+                case "Add Roles":
+                    addRoles();
+                    break;
+                case "Add Employees":
+                    addEmployees();
+                    break;
+                case "Update Employee Roles":
+                    updateEmployeeRoles();
+                    break;
+                case "Exit":
+                    connection.end();
+                    break;
+            }
+        });
+}
